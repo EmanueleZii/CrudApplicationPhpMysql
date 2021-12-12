a5a6181929a5fac655c2072e150ad51c24c0e434
Emanuele Zignale <77202606+EmanueleZii@users.noreply.github.com>
2021-12-12 12:13:15 +0100

    Update Insert.php

--- Insert.php
+++ Insert.php
@@ -1,11 +1,11 @@
 <?php
 include "Connect.php";
 
-$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-$email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
-$mobile = filter_var($_POST['mobile'], FILTER_SANITIZE_STRING);
-$password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
-$pass = sha1($password);
+@$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+@$email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+@$mobile = filter_var($_POST['mobile'], FILTER_SANITIZE_STRING);
+@$password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+@$pass = sha1($password);
 
 if (isset($_POST['submit']))
  {
